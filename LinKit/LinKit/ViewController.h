15b5ba2be03263584ac385a9fc3b789abd868ddd
ZhangWentong <zhangwentong@ZhangWentongdeMac-mini.local>
2016-10-19 03:28:39 -0700

    LinKit init

---
+++ LinKit/LinKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinKit
+//
+//  Created by ZhangWentong on 16/10/19.
+//  Copyright © 2016年 Huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LinKit/LinKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LinKit
+//
+//  Created by ZhangWentong on 16/10/19.
+//  Copyright © 2016年 Huawei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
